69103806d487989a2194b689079889c4d8b0ab52
dilan <dirac.dc@gmail.com>
2019-12-03 18:11:59 +0000

    added fb prophet tool

--- dilans_utils/plotting.py
+++ dilans_utils/plotting.py
@@ -27,3 +27,32 @@ def label(ax, xlabel, ylabel, title,  x_fs=20, y_fs=20, t_fs=30):
     ax.set_title(title)
     return ax
 
+def plot_residuals(residuals, axes=None):
+    if not axes:
+        fig, axes = prepfg(1,2)
+    
+    # create residuals timeseries
+    _ = label(axes[0], "Month", "Error", "Residuals Plot")
+    
+    outliers = {}
+    
+    # normal dbn outliers
+    cutoff = residuals.std()*1.96
+    axes[0].axhline(y=cutoff, color="r", ls="dashed", label="Normality Assumptions")
+    axes[0].axhline(y=-cutoff, color="r", ls="dashed", label="")
+    n_mask = ((residuals > cutoff) | (residuals < -cutoff))
+    outliers["normal_outliers"] = res[n_mask]
+    axes[0].scatter(residuals[n_mask].index, residuals[n_mask].values,  color="red", label="Normal Dbn Outliers",  alpha=0.5, s=100)
+
+    # twitter anomaly outiers
+    out = tad.anomaly_detect_vec(residuals.dropna(), period=12, max_anoms=0.02, direction="pos", plot=True)
+    axes[0].scatter(out.index, out.values, color="green", label="Twitter Anomaly Detector",  alpha=0.5, s=200)
+    axes[0].legend(fontsize=20)
+    outliers["twitter_anomaly_detection"] = out
+    
+    # create residual histogram
+    _ = residuals.hist(ax=axes[1])
+    _ = label(axes[1], "Error", "Density", "Residuals Hist Plot")
+    
+    return outliers, axes
+
